1db625f58cc2096cece94b5b9e33f1fb7da188bb
Ronny Czerkus <ronny.czerkus@inlog.com>
2019-07-16 11:23:37 -0300

    Primeiro Commit Aplicação WebCore MVC Crua

---
+++ dotNet-Autenticador/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotNet_Autenticador.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
